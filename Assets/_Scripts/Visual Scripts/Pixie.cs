c3789dc77f242957034294d50227217fc8805a80
chrysly <chrysly0816@gmail.com>
2024-04-22 02:02:54 -0400

    Pixie visuals implementation

---
+++ Assets/_Scripts/Visual Scripts/Pixie.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Pixie : MonoBehaviour {
+    [SerializeField] private Pixie connectionPoint;
+    [SerializeField] private Color connectionColor = Color.white;
+
+    private Transform _actorDisplay;
+    private Transform _vfxDisplay;
+    private LineRenderer _lineDisplay;
+
+    private bool _connected = false;
+
+    private void Awake() {
+        _actorDisplay = transform.GetChild(0);
+        _vfxDisplay = transform.GetChild(1);
+        _lineDisplay = transform.GetComponentInChildren<LineRenderer>();
+        _vfxDisplay.gameObject.SetActive(false);
+        _lineDisplay.materials[0].SetFloat("_Scroll" , 0f);
+        _lineDisplay.positionCount = 2;
+    }
+
+    public Color GetColor() {
+        return connectionColor;
+    }
+
+    public void ConnectPixie(Pixie target) {
+        _lineDisplay.gameObject.SetActive(true);
+        _lineDisplay.a
+    }
+
+    public void DisconnectPixie() {
+        
+    }
+}
