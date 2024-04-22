1cb57dbb28bbea98e734b480cc09dced6361b655
chrysly <chrysly0816@gmail.com>
2024-04-22 02:48:12 -0400

    DoTween import

--- Assets/_Scripts/Actor Scripts/Behaviour Scripts/Pathfinding.cs
+++ Assets/_Scripts/Actor Scripts/Behaviour Scripts/Pathfinding.cs
@@ -12,7 +12,18 @@ public class Pathfinding
         int distanceX = Mathf.Abs(tileA.x - tileB.x);
         int distanceY = Mathf.Abs(tileA.y - tileB.y);
 
-        if (distanceX > distanceY) return 1;
-        return 2;
+        if (distanceX > distanceY) return distanceY * (distanceX - distanceY);
+        return distanceX * (distanceY - distanceX);
+    }
+
+    private List<Tile> TracePath(Tile start, Tile target) {
+        List<Tile> path = new List<Tile>();
+        Tile currTile = target;
+
+        while (currTile != start) {
+            path.Add(currTile);
+        }
+
+        return null;
     }
 }
