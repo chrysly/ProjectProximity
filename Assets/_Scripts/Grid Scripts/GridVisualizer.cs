6b91ab2a00176e436bda6fd98158d0f1a76c614c
chrysly <chrysly0816@gmail.com>
2024-04-23 02:31:19 -0400

    Grid Visualizer class

--- Assets/_Scripts/Grid Scripts/GridVisualizer.cs
+++ Assets/_Scripts/Grid Scripts/GridVisualizer.cs
@@ -1,13 +1,41 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GridVisualizer : MonoBehaviour {
 
-    private Tile[,] _grid;
-    
-    private void Awake() {
+    [SerializeField] private MouseManager cursorManager;
+
+    [SerializeField] private Material selectMaterial;
+    [SerializeField] private Material scrollMaterial;
+
+    private Tile _activeTile;
+    private List<Tile> _path;
+
+    private void SelectTile(Tile source, Tile target) {
+        _activeTile = source;
+        Pathfinding pathfinder = new Pathfinding();
+        _path = pathfinder.CalculatePath(source, target, GridManager.Instance.GetGrid());
+        foreach (Tile tile in _path) {
+            tile.GetComponent<MeshRenderer>().materials[1].SetColor("_Color", Color.yellow);
+            tile.GetComponent<MeshRenderer>().materials[1].SetFloat("_Alpha", 1f);
+        }
         
+        _activeTile.GetComponent<MeshRenderer>().materials[1].SetColor("_Color", Color.green);
+        _activeTile.GetComponent<MeshRenderer>().materials[1].SetFloat("_Alpha", 1f);
+    }
+
+    private void ClearTiles() {
+        _activeTile.GetComponent<MeshRenderer>().materials[1].SetFloat("_Alpha", 0f);
+        foreach (Tile tile in _path) {
+            tile.GetComponent<MeshRenderer>().materials[1].SetFloat("_Alpha", 0f);
+        }
+    }
+
+    private void DeselectTile(Tile source, Tile target) {
+        _activeTile = source;
     }
 }
